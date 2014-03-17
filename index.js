b1d35e74d10ca60f1e0d777f8346dc1210e823c3
Matt Edelman <medelman@paypal.com>
2014-03-17 13:51:30 -0700

    adding database configuration

--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 
 var kraken = require('kraken.next'),
     express = require('express'),
+    db = require('./lib/database'),
     app = express(),
     port = process.env.PORT || 8000;
 
 
-app.use(kraken());
-
+app.use(kraken({
+    "onconfig": function(settings, cb) {
+        var dbConfig = settings.get("databaseConfig");
+        db.config(dbConfig);
+        cb(null, settings);
+    }
+}));
 
 app.listen(port, function () {
     console.log('Listening on http://127.0.0.1:8000/');
